7d0275040532b99ceb70e650ece7e5d7b9551c5d
sarath169 <rayala@iiitkalyani.ac.in>
2021-08-09 15:34:49 +0530

    manage messages

--- apps/chat/urls.py
+++ apps/chat/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('<str:room_name>/', views.room, name='room'),
+    path('api/newmessage/', views.NewMessageView.as_view(), name = 'newmessage'),
+    path('api/messages/<int:group_id>', views.GetMessageView.as_view(), name = 'messages'),
 ]
--- apps/chat/views.py
+++ apps/chat/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from rest_framework import serializers, status
+from rest_framework.response import Response
+from rest_framework.generics import CreateAPIView, ListAPIView
+from rest_framework.views import APIView
+from .serializers import MessageSerializer
+
+from .models import Message
 
 
 def index(request):
@@ -9,3 +16,21 @@ def room(request, room_name):
     return render(request, 'chatroom.html', {
         'room_name': room_name
     })
+
+
+class NewMessageView(CreateAPIView):
+    serializer_class = MessageSerializer
+
+class GetMessageView(APIView):
+    serializer_class = MessageSerializer
+    def get(self,request,group_id):
+            try:
+                    print(group_id)
+                    result = Message.objects.filter(to_group_id = group_id)
+                    serializer = MessageSerializer(result, many = True)
+                    print(serializer.data)
+                    return Response(serializer.data, status=status.HTTP_200_OK)
+            except:
+                    data = {"params":"please check the input parameters"}
+                    return Response(data, status = status.HTTP_400_BAD_REQUEST)
+    
